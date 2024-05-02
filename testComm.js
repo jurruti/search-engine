66a8a3670dd6ac7b6e94fc014d537b582845801b
Min Seong Kang <minseongkang@ms.devices.brown.edu>
2024-05-01 21:00:34 -0400

    t2

--- testComm.js
+++ testComm.js
@@ -1,4 +1,18 @@
-global.nodeConfig = {ip: '172.31.26.178', port: 8080};
+global.nodeConfig = {ip: '172.31.26.178', port: 8080, onStart: () => {
+    const node = {ip: '172.31.25.251', port: 8080};
+    message = [
+    'nid', // configuration
+    ];
+    remote = {node: node, service: 'status', method: 'get'};
+
+    distribution.local.comm.send(message, remote, (e,v) => {
+        if (!e) {
+            console.log(v);
+        } else {
+            console.log(e);
+        }
+    })
+}};
 
 
 const distribution = require('./distribution');
@@ -9,17 +23,3 @@ const {convert} = require('html-to-text');
 const id = distribution.util.id;
 
 
-const node = {ip: '172.31.25.251', port: 8080};
-
-message = [
-'nid', // configuration
-];
-remote = {node: node, service: 'status', method: 'get'};
-
-distribution.local.comm.send(message, remote, (e,v) => {
-    if (!e) {
-        console.log(v);
-    } else {
-        console.log(e);
-    }
-})
